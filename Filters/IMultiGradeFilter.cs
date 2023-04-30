56c71fcc3ae6b869c03e34ea720cc1dab7467769
ovhk <olivier@vanhoucke.com>
2023-04-30 22:16:16 +0200

    Add Filter & Paper interface

---
+++ Filters/IMultiGradeFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalFilm.Filters
+{
+    public interface IMultiGradeFilter
+    {
+        string Name { get; }
+    }
+}
