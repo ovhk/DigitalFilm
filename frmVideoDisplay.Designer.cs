82d93dba144a2fc9d088ec0d581b6ed4e183e11d
ovhk <olivier@vanhoucke.com>
2023-11-04 21:39:11 +0100

    Change VLC lib

---
+++ frmVideoDisplay.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DigitalFilm
+{
+    partial class frmVideoDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.Location = new System.Drawing.Point(75, 57);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(538, 322);
+            this.videoView1.TabIndex = 0;
+            this.videoView1.Text = "videoView1";
+            // 
+            // frmVideoDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.videoView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmVideoDisplay";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.frmVideoDisplay_Load);
+            this.VisibleChanged += new System.EventHandler(this.frmDisplay_VisibleChanged);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmDisplay_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmDisplay_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.frmDisplay_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+    }
+}
---
+++ frmVideoDisplay.cs
@@ -0,0 +1,178 @@
+﻿using DigitalFilm.Engine;
+using DigitalFilm.Panels;
+using LibVLCSharp.Shared;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DigitalFilm
+{
+    /// <summary>
+    /// Display Form
+    /// </summary>
+    public partial class frmVideoDisplay : Form
+    {
+        public LibVLC _libVLC;
+        public MediaPlayer _mp;
+
+        /// <summary>
+        /// Display Engine
+        /// </summary>
+        private readonly DisplayEngine engine;
+
+        /// <summary>
+        /// FrmVideoDisplay constructor
+        /// </summary>
+        public frmVideoDisplay()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true; // Needed to eliminate flickering
+            _libVLC = new LibVLC();
+            _mp = new MediaPlayer(_libVLC);
+            videoView1.MediaPlayer = _mp;
+
+
+            engine = DisplayEngine.GetInstance();
+        }
+
+        private readonly Stopwatch _sw = new Stopwatch();
+
+
+        #region Invoke Management
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        private void SafeUpdate(Action action)
+        {
+            if (this.InvokeRequired)
+            {
+                _ = this.BeginInvoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        #endregion
+
+        #region Dragging Management
+
+        /// <summary>
+        /// Dragging flag
+        /// </summary>
+        private bool dragging = false;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Point dragCursorPoint;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Point dragFormPoint;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Cursor.Current = Cursors.SizeAll;
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmDisplay_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (engine.Panel.IsFullScreen == false)
+            {
+                dragging = true;
+                dragCursorPoint = Cursor.Position;
+                dragFormPoint = this.Location;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmDisplay_MouseUp(object sender, MouseEventArgs e)
+        {
+            Cursor.Current = Cursors.Default;
+            dragging = false;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmDisplay_VisibleChanged(object sender, EventArgs e)
+        {
+            if (this.Visible)
+            {
+                // Adapte to panel size
+                if (engine.Panel is PanelSimulator) // TODO simplify this ! Put Screen in IPanel?
+                {
+                    Point p = new Point
+                    {
+                        X = Screen.PrimaryScreen.WorkingArea.Left,
+                        Y = Screen.PrimaryScreen.WorkingArea.Top
+                    };
+
+                    this.TopMost = false;
+                    this.Location = p;
+                }
+                else if (engine.Panel is ExternalPanel)
+                {
+                    ExternalPanel ep = engine.Panel as ExternalPanel;
+
+                    Point p = new Point
+                    {
+                        X = ep.Screen.WorkingArea.Left,
+                        Y = ep.Screen.WorkingArea.Top
+                    };
+
+                    this.TopMost = true;
+                    this.Location = p;
+                }
+                else if (engine.Panel is Wisecoco8k103Panel)
+                {
+                    Wisecoco8k103Panel ep = engine.Panel as Wisecoco8k103Panel;
+
+                    Point p = new Point
+                    {
+                        X = ep.Screen.WorkingArea.Left,
+                        Y = ep.Screen.WorkingArea.Top
+                    };
+
+                    this.TopMost = true;
+                    this.Location = p;
+                }
+            }
+        }
+
+        private void frmVideoDisplay_Load(object sender, EventArgs e)
+        {
+            Media media = new Media(_libVLC, "C:\\Users\\sectronic\\source\\repos\\DigitalFilm\\bin\\Debug\\cache\\tmp2\\out.avi", FromType.FromPath);
+            videoView1.MediaPlayer.Play(media);
+        }
+    }
+}
